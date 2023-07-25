7add6859dc7fc0b4fe7162027126bb7ca8e31924
hanichoye <hana36593@gmail.com>
2023-07-25 05:33:00 -0700

    AirBnB

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""starts a Flask web application
-"""
+"""starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
 
 @app.teardown_appcontext
 def close_db(exc):
@@ -16,14 +13,12 @@ def close_db(exc):
     """
     storage.close()
 
-
 @app.route('/states_list')
 def states_list():
     """display a HTML page
     """
     states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
